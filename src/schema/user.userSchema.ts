ecfee3ff7c4b836a255080723ff42a5e8af780a2
jarengisner <124743127+jarengisner@users.noreply.github.com>
2024-10-07 09:48:45 -0700

    Starting to add auth and encryption

--- src/schema/user.userSchema.ts
+++ src/schema/user.userSchema.ts
@@ -1,5 +1,7 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import * as argon from 'argon2';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -13,3 +15,27 @@ export class User {
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+/* UserSchema.statics.hashPassword = async (password: string) => {
+  try {
+    const hash = await argon.hash(password);
+
+    return hash;
+  } catch (err) {
+    throw new HttpException(
+      `Error in hashing password: ${err.message}`,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+};
+
+UserSchema.methods.validatePassword = async (password: string) => {
+  try {
+    return await argon.verify(this.password, password);
+  } catch (err) {
+    throw new HttpException(
+      `Error within validation of the password: ${err.message}`,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}; */
--- src/services/user.userService.ts
+++ src/services/user.userService.ts
@@ -5,17 +5,40 @@ import { User, UserDocument } from 'src/schema/user.userSchema';
 
 import { UserDto } from '../dto/user.userDto';
 
+import * as argon from 'argon2';
+
 @Injectable()
 export class UserService {
   constructor(@InjectModel('User') private userModel: Model<UserDocument>) {}
 
+  async hashPassword(password: string): Promise<string> {
+    try {
+      const hash = await argon.hash(password);
+
+      return hash;
+    } catch (err) {
+      throw new HttpException(
+        `Error within hashing password: ${err.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  /**
+   *
+   * @param userDto - User data transfer object, interface for user
+   * @returns Promise which resolves the newly created user
+   */
   async create(userDto: UserDto): Promise<User> {
     const newUser = new this.userModel(userDto);
 
     return newUser.save();
   }
 
-  //get all users
+  /**
+   *
+   * @returns promise of User Array
+   */
   async findAll(): Promise<any> {
     try {
       const allUsers: User[] = await this.userModel.find().exec();
@@ -33,6 +56,11 @@ export class UserService {
     }
   }
 
+  /**
+   *
+   * @param username - string username of user
+   * @returns Promise object of a User
+   */
   async findOne(username: string): Promise<User> {
     try {
       const searchUser: User = await this.userModel
